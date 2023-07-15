f11081371fa979878ff2267a5fe2db9a9804710d
qtt153759 <qtt153759@gmail.com>
2023-07-16 02:15:58 +0700

    add infor

--- scripts/index.js
+++ scripts/index.js
@@ -1,77 +1,79 @@
 const index = {};
 
-index.$menu = $('#sideMenu');
-index.$navItem = $('.navItem');
-index.$navItemName = $('.navItemName')
-index.$home = $('#home');
-index.$about=$('#about');
-index.$skills = $('#skills');
-index.$works = $('#works');
-index.$contact = $('#contact');
-index.$menuButton = $('#menuButton');
-index.$scrollDown = $('#scrollDown');
+index.$menu = $("#sideMenu");
+index.$navItem = $(".navItem");
+index.$navItemName = $(".navItemName");
+index.$home = $("#home");
+index.$about = $("#about");
+index.$skills = $("#skills");
+index.$books = $("#books");
+index.$works = $("#works");
+index.$contact = $("#contact");
+index.$menuButton = $("#menuButton");
+index.$scrollDown = $("#scrollDown");
 index.isOpen = false;
 
 // Scroll function
-index.scroll = function(target) {
-  $('html,body').animate({ scrollTop: $(target).offset().top }, 500);
-}
+index.scroll = function (target) {
+  $("html,body").animate({ scrollTop: $(target).offset().top }, 500);
+};
 
 // Menu button function
-index.showHideMenu = function() {
-  index.$menu.toggleClass('sideMenuHide sideMenuShow');
+index.showHideMenu = function () {
+  index.$menu.toggleClass("sideMenuHide sideMenuShow");
   index.isOpen = !index.isOpen;
-}
+};
 
 // Mobile class changes on initial load
 if ($(window).width() <= 990) {
-  index.$menu.addClass('sideMenuHide').removeClass('sideMenuShow');
+  index.$menu.addClass("sideMenuHide").removeClass("sideMenuShow");
   index.$scrollDown.hide();
 }
 
-index.eventListeners = function() {
+index.eventListeners = function () {
   // when windox resizes between large and small displayes
-  $(window).on('resize', function() {
+  $(window).on("resize", function () {
     if ($(window).width() > 990) {
-      index.$menu.removeClass('sideMenuHide').addClass('sideMenuShow')
+      index.$menu.removeClass("sideMenuHide").addClass("sideMenuShow");
       index.$scrollDown.show();
-      index.isOpen= false;
+      index.isOpen = false;
     } else {
-      index.$menu.removeClass('sideMenuShow').addClass('sideMenuHide');
+      index.$menu.removeClass("sideMenuShow").addClass("sideMenuHide");
       index.isOpen = false;
       index.$scrollDown.hide();
     }
   });
 
   // Nav menu click events for scroll function
-  $('a[href*=\\#]').on('click', function () {
+  $("a[href*=\\#]").on("click", function () {
     index.scroll(this.hash);
   });
 
   // Menu button click event
-  index.$menuButton.on('click', index.showHideMenu);
+  index.$menuButton.on("click", index.showHideMenu);
 
   // Hide menu when clicking a link
-  index.$navItem.on('click', function() {
-    if (index.isOpen) {// Hide menu when clicking a link
+  index.$navItem.on("click", function () {
+    if (index.isOpen) {
+      // Hide menu when clicking a link
       index.showHideMenu();
     }
   });
 
   // Menu button ENTER key event
-  index.$menuButton.on('keypress', function(e){
+  index.$menuButton.on("keypress", function (e) {
     if (e.which === 13) {
-      $(this).trigger('click');
+      $(this).trigger("click");
     }
-  })
-}
+  });
+};
 
 // init method
-index.init = function() {
+index.init = function () {
   index.eventListeners();
-}
+};
 
 // Document Ready
-$(function() {
+$(function () {
   index.init();
-})
+});
